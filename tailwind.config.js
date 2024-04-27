f6aa5471a070e21bf628b39ca83a429eb7bfe79c
YASIN1881 <yasinnasiry18@gmail.com>
2024-04-27 21:31:38 +0330

    complete table of purchase.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,11 @@ module.exports = {
         backgroundColor: "#e9ecef",
         backgroundColorItem: "#0e132d",
         borderColor: "#242a48",
-        blueColor: "#556fff"
+        blueColor: "#556fff",
+        greenText: "#00C9A7",
+        greenBg: "#00C9A71A",
+        redText: "#ED4C78",
+        redBg: "#ED4C781A"
       }
     },
   },
